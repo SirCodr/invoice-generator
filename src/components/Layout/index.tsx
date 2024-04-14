01eb4c89707f10da6c558d0dbefc857b48ab9194
SirCodr <jotadeveloper00@gmail.com>
2024-04-14 12:51:52 -0500

    Remove header from layout component

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,21 +1,6 @@
-import { Button } from 'primereact/button'
-import { useInvoiceStore } from '../../store/invoiceStore'
-
 const Layout = ({ children }: { children: JSX.Element | JSX.Element[] }) => {
-  const [preview, togglePreview] = useInvoiceStore((state) => [
-    state.preview,
-    state.togglePreview
-  ])
-
   return (
     <main className='flex flex-col gap-y-4 h-screen'>
-      <header>
-        <Button
-          type='button'
-          label={preview ? 'Ocultar' : 'Mostrar'}
-          onClick={togglePreview}
-        />
-      </header>
       {children}
     </main>
   )
